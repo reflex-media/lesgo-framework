d5dcd76f79b2a9ba815896f4490520a33f59c44f
Mohd Sufiyan <mosufy@gmail.com>
2019-10-05 15:40:21 +0800

    getObject as a util function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,6 @@
 import logger from './logger';
-import objectStore from './objectStore';
+import { getObject } from './objectStore';
 import { dispatch } from './queue';
 
 // eslint-disable-next-line import/prefer-default-export
-export { logger, dispatch, objectStore };
+export { logger, dispatch, getObject };
