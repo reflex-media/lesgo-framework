34898bd614ace6bd840556862148fe1ba7ae2b40
Peter Indiola <peter@incube8.sg>
2019-11-01 09:36:40 +0800

    Move other functions and refactoring

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ exports.printMsg = function printr() {
   // eslint-disable-next-line no-console
   console.log('Import specific middlewares as required.');
 };
+
+const middlewares = require('./build/middlewares');
+
+exports.middlewares = middlewares;
+
+const services = require('./build/services');
+
+exports.services = services;
