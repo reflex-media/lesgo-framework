528e40878d949da128d4cfe89f8ab6c34d0a966a
Daison Cariño <daison12006013@gmail.com>
2019-11-14 14:33:47 +0800

    Fix mocking of config

--- setupTest.js
+++ setupTest.js
@@ -1,10 +1,5 @@
 /* eslint no-console: 0 */
 
-// Add test-specific environment configurations
-process.env.APP_ENV = 'test';
-process.env.APP_DEBUG = true;
-process.env.SENTRY_ENABLED = true;
-
 // Mock the console logs
 console.log = jest.fn();
 console.debug = jest.fn();
