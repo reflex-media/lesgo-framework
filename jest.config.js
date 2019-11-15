4c24b1d84e65b8ea4bebd8b588052a977a6c9ddf
Sufiyan Rahmat <sufiyan@incube8.sg>
2019-11-15 15:54:52 +0800

    Remove coverageTreshold error reporting

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,14 @@ module.exports = {
   testMatch: ['**/__tests__/*.spec.js'],
   collectCoverageFrom: ['src/**/*.js'],
   coverageReporters: ['html', 'text', 'lcov'],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 100,
+  //     functions: 100,
+  //     lines: 100,
+  //     statements: 100,
+  //   },
+  // },
   setupFiles: ['./setupTest.js'],
   moduleNameMapper: {
     '^Config(.*)$': '<rootDir>/tests/__mocks__/config$1.js',
