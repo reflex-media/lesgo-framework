df9f2e0d4c479b85343d579c2954190accb9f347
Joshua Paylaga <joshuapaylaga@gmail.com>
2021-01-28 12:14:36 +0800

    Fix jsdoc

--- src/services/pagination/Paginator.js
+++ src/services/pagination/Paginator.js
@@ -58,7 +58,7 @@ export default class Paginator {
   /**
    * Previous page
    *
-   * @returns {null|number}
+   * @returns {boolean|number}
    */
   previousPage() {
     if (this.currentPage() > 1) {
