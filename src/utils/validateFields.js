813c8f45b2b5a6a2059cbf55c0bd5a92eabeab6f
Ellen Ortiz <ellen051394@gmail.com>
2021-02-24 10:23:56 +0800

    Add validate fields utils for validating field type along side with is email util for checking email format (#36)
    Co-authored-by: Ellen Ortiz <ellen@iona.studio>

---
+++ src/utils/validateFields.js
@@ -0,0 +1,53 @@
+import LesgoException from '../exceptions/LesgoException';
+import isEmail from './isEmail';
+
+const FILE = 'Utils/validateFields';
+
+export default (params, validFields) => {
+  const validated = {};
+
+  validFields.forEach(field => {
+    if (field.required && !params[field.key]) {
+      throw new LesgoException(
+        `Missing required '${field.key}'`,
+        `${FILE}::MISSING_REQUIRED_${field.key.toUpperCase()}`,
+        500,
+        { field }
+      );
+    }
+
+    if (
+      (field.type === 'string' &&
+        typeof params[field.key] !== 'undefined' &&
+        typeof params[field.key] !== 'string') ||
+      (field.type === 'object' &&
+        typeof params[field.key] !== 'undefined' &&
+        typeof params[field.key] !== 'object') ||
+      (field.type === 'number' &&
+        typeof params[field.key] !== 'undefined' &&
+        typeof params[field.key] !== 'number') ||
+      (field.type === 'email' &&
+        typeof params[field.key] !== 'undefined' &&
+        !isEmail(params[field.key])) ||
+      (field.type === 'array' &&
+        typeof params[field.key] !== 'undefined' &&
+        (!Array.isArray(params[field.key]) || !params[field.key].length)) ||
+      (field.type === 'enum' &&
+        typeof params[field.key] !== 'undefined' &&
+        !field.enumValues.includes(params[field.key]))
+    ) {
+      throw new LesgoException(
+        `Invalid type for '${field.key}', expecting '${field.type}'`,
+        `${FILE}::INVALID_TYPE_${field.key.toUpperCase()}`,
+        500,
+        { field, value: params[field.key] }
+      );
+    }
+
+    if (typeof params[field.key] !== 'undefined') {
+      validated[field.key] = params[field.key];
+    }
+  });
+
+  return validated;
+};
