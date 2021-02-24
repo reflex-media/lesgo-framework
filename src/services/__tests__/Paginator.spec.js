ddff47436fafe2e38be0ff3232f6b88a0e8e7f14
Joshua Paylaga <joshuapaylaga@gmail.com>
2021-02-24 10:35:50 +0800

    Feature/paginate (#35)
    * Pagination helper initial commit
    
    * Removed wrongly committed directory
    
    * Created mock db object and fixed countData
    
    * Initial commit for the pagination classes
    
    * Add unit test
    
    * Create unit test for LengthAwarePaginator
    
    * Add toObject() method
    
    * Implement toObject, previousPage, and nextPage
    
    * Implement toObject on LengthAwarePagination
    
    * Created utils for pagination
    
    * Update utils/paginate unit test
    
    * Add comments
    
    * Update comment on paginate function
    
    * Use dependency injection
    
    * Make perPage optional
    
    * Fix formatting
    
    * Fix formatting
    
    * Fix jsdoc
    
    * Fix default values in parameters
    
    * Handle null values on utils
    
    * Created db.selectPaginate
    
    * Apply suggestion on unit testing

---
+++ src/services/__tests__/Paginator.spec.js
@@ -0,0 +1,179 @@
+import Paginator from '../pagination/Paginator';
+import {
+  mockData,
+  mockDataFirstItem,
+  mockDataLastItem,
+} from '../../utils/__mocks__/db';
+import LesgoException from '../../exceptions/LesgoException';
+import db from '../../utils/db';
+
+jest.mock('../../utils/db');
+
+beforeEach(() => {
+  db.select.mockClear();
+});
+
+const FILE = 'Services/pagination/Paginator';
+
+describe('test Paginator instantiate', () => {
+
+  it.each`
+    page         | perPage      | expectedCount | firstItem            | lastItem
+    ${undefined} | ${undefined} | ${10}         | ${mockData}          | ${mockData}
+    ${undefined} | ${5}         | ${5}          | ${mockDataFirstItem} | ${mockDataLastItem}
+  `(
+    'should not throw exception if page is $page and perPage is $perPage',
+    async ({
+       page,
+       perPage,
+       expectedCount,
+       firstItem,
+       lastItem,
+     }) => {
+      const paginator = new Paginator(db, 'SELECT * FROM tests', {}, perPage, page);
+
+      expect(await paginator.count()).toEqual(expectedCount);
+      expect(paginator.currentPage()).toEqual(1);
+      expect(await paginator.firstItem()).toMatchObject(firstItem);
+      expect(await paginator.lastItem()).toMatchObject(lastItem);
+      expect(paginator.perPage()).toEqual(expectedCount);
+    }
+  );
+
+  it.each`
+    page         | perPage      | errorName           | errorMessage                        | errorCode                               | errorStatusCode
+    ${'sample'}  | ${10}        | ${'LesgoException'} | ${"Invalid type for 'currentPage'"} | ${`${FILE}::INVALID_TYPE_CURRENT_PAGE`} | ${500}
+    ${1}         | ${'sample'}  | ${'LesgoException'} | ${"Invalid type for 'perPage'"}     | ${`${FILE}::INVALID_TYPE_PER_PAGE`}     | ${500}
+  `(
+    'should throw $errorMessage when page is $page and perPage is $perPage',
+    ({
+       page,
+       perPage,
+       errorName,
+       errorMessage,
+       errorCode,
+       errorStatusCode,
+     }) => {
+      try {
+        const values = new Paginator(db, 'SELECT * FROM tests', {}, perPage, page)
+        expect(values).toThrow();
+      } catch (err) {
+        expect(err.name).toEqual(errorName);
+        expect(err.message).toEqual(errorMessage);
+        expect(err.code).toEqual(errorCode);
+        expect(err.statusCode).toEqual(errorStatusCode);
+      }
+    }
+  );
+
+  it('should return the first item of the result', async () => {
+    const paginator = new Paginator(db, 'SELECT * FROM tests', {}, 5);
+
+    expect(await paginator.firstItem()).toMatchObject(mockDataFirstItem);
+  });
+  it('should return the last item of the result', async () => {
+    const paginator = new Paginator(db, 'SELECT * FROM tests', {}, 5);
+
+    expect(await paginator.lastItem()).toMatchObject(mockDataLastItem);
+  });
+  it('should return the defined per page', () => {
+    const paginator = new Paginator(db, 'SELECT * FROM tests', {}, 5);
+
+    expect(paginator.perPage()).toEqual(5);
+  });
+  it('should return the previous page if exist', () => {
+    const paginator1 = new Paginator(db, 'SELECT * FROM tests', {}, 5, 2);
+    expect(paginator1.previousPage()).toEqual(1);
+
+    const paginator2 = new Paginator(db, 'SELECT * FROM tests', {}, 5, 1);
+    expect(paginator2.previousPage()).toEqual(false);
+  });
+  it('should return the next page if exist', async () => {
+    const paginator1 = new Paginator(db, 'SELECT * FROM tests', {}, 5);
+    expect(await paginator1.nextPage()).toEqual(2);
+
+    const paginator2 = new Paginator(db, 'SELECT * FROM tests', {}, 5, 2);
+    expect(await paginator2.nextPage()).toEqual(3);
+
+    const paginator3 = new Paginator(db, 'SELECT * FROM tests', {}, 5, 3);
+    expect(await paginator3.nextPage()).toEqual(false);
+  });
+});
+
+describe('test count() usage', () => {
+  it('should count number of items of the current page', async () => {
+    const paginator = new Paginator(db, 'SELECT * FROM tests', {}, 5);
+
+    expect(await paginator.count()).toEqual(5);
+  });
+  it('should only run executeQuery once', async () => {
+    const paginator = new Paginator(db, 'SELECT * FROM tests', {}, 5);
+
+    await paginator.count();
+    await paginator.count();
+    await paginator.count();
+    await paginator.count();
+    await paginator.count();
+
+    expect(db.select).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('test currentPage() usage', () => {
+  it('should return 1 as current page if no currentPage is defined', () => {
+    const paginator = new Paginator(db, 'SELECT * FROM tests', {}, 5);
+
+    expect(paginator.currentPage()).toEqual(1);
+  });
+  it('should return the defined current page on instantiation', () => {
+    const paginator = new Paginator(db, 'SELECT * FROM tests', {}, 5, 2);
+
+    expect(paginator.currentPage()).toEqual(2);
+  });
+});
+
+describe('test items() usage', () => {
+  it('should return all results from the current page', async () => {
+    const paginator = new Paginator(db, 'SELECT * FROM tests', {}, 5);
+
+    expect(await paginator.items()).toMatchObject([
+      { ...mockDataFirstItem },
+      { ...mockData },
+      { ...mockData },
+      { ...mockData },
+      { ...mockDataLastItem },
+    ]);
+  });
+
+  it('should only run executeQuery once', async () => {
+    const paginator = new Paginator(db, 'SELECT * FROM tests', {}, 5);
+
+    await paginator.items();
+    await paginator.items();
+    await paginator.items();
+    await paginator.items();
+
+    expect(db.select).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('test toObject() usage', () => {
+  it('should return object version of the paginator', async () => {
+    const paginator = new Paginator(db, 'SELECT * FROM tests', {}, 5);
+
+    expect(await paginator.toObject()).toMatchObject({
+      count: 5,
+      previous_page: false,
+      current_page: 1,
+      next_page: 2,
+      per_page: 5,
+      items: [
+        { ...mockDataFirstItem },
+        { ...mockData },
+        { ...mockData },
+        { ...mockData },
+        { ...mockDataLastItem },
+      ],
+    });
+  });
+});
---
+++ src/services/pagination/Paginator.js
@@ -0,0 +1,196 @@
+import LesgoException from '../../exceptions/LesgoException';
+
+const FILE = 'Services/pagination/Paginator';
+
+export default class Paginator {
+  /**
+   * Constructor
+   *
+   * @param db
+   * @param sql
+   * @param sqlParams
+   * @param perPage
+   * @param currentPage
+   */
+  constructor(db, sql, sqlParams, perPage = 10, currentPage = 1) {
+    if (typeof perPage !== 'number') {
+      throw new LesgoException(
+        "Invalid type for 'perPage'",
+        `${FILE}::INVALID_TYPE_PER_PAGE`,
+        500,
+        { perPage }
+      );
+    }
+
+    if (typeof currentPage !== 'number') {
+      throw new LesgoException(
+        "Invalid type for 'currentPage'",
+        `${FILE}::INVALID_TYPE_CURRENT_PAGE`,
+        500,
+        { currentPage }
+      );
+    }
+
+    this.dbProp = db;
+    this.sqlProp = sql;
+    this.sqlParamsProp = sqlParams;
+    this.perPageProp = perPage;
+    this.currentPageProp = currentPage;
+
+    this.hasNext = false;
+
+    this.response = [];
+  }
+
+  /**
+   * Count all items in the current page.
+   *
+   * @returns {number}
+   */
+  async count() {
+    if (this.response.length <= 0) {
+      await this.executeQuery();
+    }
+
+    return this.response.length;
+  }
+
+  /**
+   * Previous page
+   *
+   * @returns {boolean|number}
+   */
+  previousPage() {
+    if (this.currentPage() > 1) {
+      return this.currentPage() - 1;
+    }
+
+    return false;
+  }
+
+  /**
+   * The current page.
+   *
+   * @returns {*|number}
+   */
+  currentPage() {
+    return this.currentPageProp;
+  }
+
+  /**
+   * Next page
+   *
+   * @returns {Promise<boolean|*>}
+   */
+  async nextPage() {
+    if (this.response.length <= 0) {
+      await this.executeQuery();
+    }
+
+    if (this.hasNext) {
+      return this.currentPage() + 1;
+    }
+
+    return false;
+  }
+
+  /**
+   * First item in the current page.
+   *
+   * @returns {*}
+   */
+  async firstItem() {
+    if (this.response.length <= 0) {
+      await this.executeQuery();
+    }
+
+    return this.response[0];
+  }
+
+  /**
+   * Last item in the current page.
+   */
+  async lastItem() {
+    if (this.response.length <= 0) {
+      await this.executeQuery();
+    }
+
+    return this.response[this.response.length - 1];
+  }
+
+  /**
+   * Number of items per page.
+   *
+   * @returns {*}
+   */
+  perPage() {
+    return this.perPageProp;
+  }
+
+  /**
+   * All items in the current page.
+   *
+   * @returns {[]}
+   */
+  async items() {
+    if (this.response.length <= 0) {
+      await this.executeQuery();
+    }
+
+    return this.response;
+  }
+
+  /**
+   * Convert to object.
+   *
+   * @returns {Promise<{per_page: *, count: *, items: *, current_page: (*|number)}>}
+   */
+  async toObject() {
+    if (this.response.length <= 0) {
+      await this.executeQuery();
+    }
+
+    return {
+      count: await this.count(),
+      previous_page: this.previousPage(),
+      current_page: this.currentPage(),
+      next_page: await this.nextPage(),
+      per_page: this.perPage(),
+      items: await this.items(),
+    };
+  }
+
+  // They act as protected methods.
+
+  getLimitAndOffsetByPageAndContentPerPage() {
+    const offset = this.currentPage() * this.perPage() - this.perPage();
+    const limit = this.perPage();
+
+    return {
+      offset,
+      limit,
+    };
+  }
+
+  generatePaginationSqlSnippet() {
+    const values = this.getLimitAndOffsetByPageAndContentPerPage();
+    const limitWithExtraData = values.limit + 1;
+    return this.sqlProp.concat(
+      ` LIMIT ${limitWithExtraData} OFFSET ${values.offset}`
+    );
+  }
+
+  async executeQuery() {
+    this.response = await this.dbProp.select(
+      this.generatePaginationSqlSnippet(),
+      this.sqlParamsProp
+    );
+
+    this.hasNext = this.response.length > this.perPage();
+    if (this.hasNext) {
+      this.response.pop();
+    }
+
+    return this.response;
+  }
+}
