3c6bdd8c9e9d4726ac0649e201c2bfc1b66d0b7b
Sufiyan Rahmat <sufiyan@incube8.sg>
2024-09-10 16:29:02 +0800

    disconnectCache utils

---
+++ src/utils/cache/redis/disconnectCache.ts
@@ -0,0 +1,7 @@
+import { disconnectElastiCacheRedisClient } from '../../../services/ElastiCacheRedisService';
+
+const disconnectCache = () => {
+  return disconnectElastiCacheRedisClient();
+};
+
+export default disconnectCache;
