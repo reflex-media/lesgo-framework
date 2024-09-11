88f2ceb9f4f924f41c5b7ca414720502454c8eb2
Sufiyan Rahmat <sufiyan@incube8.sg>
2024-09-11 16:49:47 +0800

    boyscouting for lowercase fix of secretsmanager from secretsManager

--- dist/config/secretsmanager2.d.ts
+++
@@ -1,4 +0,0 @@
-declare const _default: {
-    region: string;
-};
-export default _default;
--- dist/config/secretsmanager2.js
+++
@@ -1,4 +0,0 @@
-import awsConfig from './aws';
-export default {
-  region: process.env.LESGO_AWS_SECRETS_MANAGER_REGION || awsConfig.region,
-};
