4a02321319111ec659382ccf2736ab454a08bbbf
Mohamed Khan <mkhan377@gmail.com>
2024-10-10 08:55:52 +0800

    Add reference to jsdocs

--- src/utils/safePromise.ts
+++ src/utils/safePromise.ts
@@ -2,14 +2,19 @@
  *
  * @param promise
  * @returns [err, res]
+ * @reference https://github.com/arthurfiorette/proposal-safe-assignment-operator
  *
  * @example
+ * ```typescript
+ * import { safePromise } from 'lesgo/utils';
+ *
  * const [err, res] = await safePromise(fetch('https://example.com/'));
  * if (err) {
  *   console.error(err);
  * } else {
  *   console.log(res);
  * }
+ * ```
  */
 const safePromise = async <T>(
   promise: Promise<T>
