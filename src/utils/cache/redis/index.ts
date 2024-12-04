29db1529929538d4d101125a40b8e5b67d09ecb5
Sufiyan Rahmat <sufiyan@incube8.sg>
2024-12-04 13:25:53 +0800

    New feature scanRedisCache

--- src/utils/cache/redis/index.ts
+++ src/utils/cache/redis/index.ts
@@ -2,4 +2,5 @@ export { default as disconnectCache } from './disconnectCache';
 export { default as deleteCache } from './deleteCache';
 export { default as getCache } from './getCache';
 export { default as getClient } from './getClient';
+export { default as scanCache } from './scanCache';
 export { default as setCache } from './setCache';
