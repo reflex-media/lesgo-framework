5b3e2eb5e33ce653b52e6c77315d861140b34f07
Sufiyan Rahmat <sufiyan@incube8.sg>
2024-12-10 10:02:10 +0800

    Deprecated disconnect db function due to the use of ConnectionPool as this also disconnects the pool for other concurrent invocations with Singleton connection

--- src/services/RDSAuroraMySQLProxyService/disconnectMySQLProxyClient.ts
+++ src/services/RDSAuroraMySQLProxyService/disconnectMySQLProxyClient.ts
@@ -1,29 +1,31 @@
 import { logger } from '../../utils';
-import { singleton } from '../RDSAuroraMySQLProxyService/getMySQLProxyClient';
 
 const FILE =
   'lesgo.services.RDSAuroraMySQLProxyService.disconnectMySQLProxyClient';
 
+// @deprecated Disconnect db is no longer to be used due to the use of ConnectionPool
 const disconnectMySQLProxyClient = async () => {
-  const singletonConns = Object.keys(singleton);
-  if (singletonConns.length === 0) {
-    logger.debug(`${FILE}::NO_CONNECTIONS_TO_DISCONNECT`);
-    return;
-  }
+  logger.warn(`${FILE}::DEPRECATED_FUNCTION_DO_NOT_END_POOL_CONNECTION`);
 
-  logger.debug(`${FILE}::PREPARING_TO_DISCONNECT`, {
-    singletonConns,
-  });
+  // const singletonConns = Object.keys(singleton);
+  // if (singletonConns.length === 0) {
+  //   logger.debug(`${FILE}::NO_CONNECTIONS_TO_DISCONNECT`);
+  //   return;
+  // }
 
-  singletonConns.forEach(async singletonConn => {
-    try {
-      await singleton[singletonConn].end();
-      delete singleton[singletonConn];
-      logger.debug(`${FILE}::COMPLETED`, { singletonConn });
-    } catch (err) {
-      logger.error(`${FILE}::ERROR`, { singletonConn, err });
-    }
-  });
+  // logger.debug(`${FILE}::PREPARING_TO_DISCONNECT`, {
+  //   singletonConns,
+  // });
+
+  // singletonConns.forEach(async singletonConn => {
+  //   try {
+  //     await singleton[singletonConn].end();
+  //     delete singleton[singletonConn];
+  //     logger.debug(`${FILE}::COMPLETED`, { singletonConn });
+  //   } catch (err) {
+  //     logger.error(`${FILE}::ERROR`, { singletonConn, err });
+  //   }
+  // });
 };
 
 export default disconnectMySQLProxyClient;
