acdb96bc3c16ec4a2a23c123995c8309c05b2e4a
Mohamed Khan <mkhan377@gmail.com>
2025-02-03 11:52:42 +0800

    Add typeSafePromise function

---
+++ src/utils/typeSafePromise.ts
@@ -0,0 +1,48 @@
+/**
+ *
+ * @param promise
+ * @returns {success: true, data: TData} | {success: false, error: TError}
+ * @reference https://github.com/arthurfiorette/proposal-safe-assignment-operator
+ *
+ * @example
+ * ```typescript
+ * import { safePromise } from 'lesgo/utils';
+ *
+ * const res = await safePromise(fetch('https://example.com/'));
+ * if (res.success) {
+ *   console.log(res.data);
+ * } else {
+ *   console.error(res.error);
+ * }
+ * ```
+ */
+type ReturnTypeSafePromise<TData, TError> =
+  | {
+      success: true;
+      data: TData;
+      error?: undefined;
+    }
+  | {
+      success: false;
+      data?: undefined;
+      error: TError;
+    };
+
+const typeSafePromise = async <TData, TError = Error>(
+  promise: Promise<TData>
+): Promise<ReturnTypeSafePromise<TData, TError>> => {
+  try {
+    const res = await promise;
+    return {
+      success: true,
+      data: res,
+    };
+  } catch (err) {
+    return {
+      success: false,
+      error: err as TError,
+    };
+  }
+};
+
+export default typeSafePromise;
