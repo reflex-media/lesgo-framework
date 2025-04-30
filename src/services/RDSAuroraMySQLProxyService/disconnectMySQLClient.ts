f24b14ac9d3980142fe9b7b0285cdf97d38db28f
xXLXx <leo.lavarias@gmail.com>
2025-04-30 11:23:48 +0800

    feat: add mysqlclient disconnect

---
+++ src/services/RDSAuroraMySQLProxyService/disconnectMySQLClient.ts
@@ -0,0 +1,26 @@
+import { logger } from '../../utils';
+import { singleton } from './getMySQLProxyClient';
+
+const FILE = 'lesgo.services.RDSAuroraMySQLProxyService.disconnectMySQLClient';
+
+const disconnectMySQLClient = async () => {
+  const singletonConns = Object.keys(singleton);
+  if (singletonConns.length === 0) {
+    logger.debug(`${FILE}::NO_CONNECTIONS_TO_DISCONNECT`);
+    return;
+  }
+  logger.debug(`${FILE}::PREPARING_TO_DISCONNECT`, {
+    singletonConns,
+  });
+  singletonConns.forEach(async singletonConn => {
+    try {
+      await singleton[singletonConn].end();
+      delete singleton[singletonConn];
+      logger.debug(`${FILE}::COMPLETED`, { singletonConn });
+    } catch (err) {
+      logger.error(`${FILE}::ERROR`, { singletonConn, err });
+    }
+  });
+};
+
+export default disconnectMySQLClient;
