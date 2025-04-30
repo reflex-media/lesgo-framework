816dd9a479bd3d314bef52c9f225ce30c03ffdc6
xXLXx <leo.lavarias@gmail.com>
2025-04-30 11:58:09 +0800

    feat: add mysql disconnect

---
+++ src/utils/db/mysql/proxy/disconnectMySQLDb.ts
@@ -0,0 +1,10 @@
+import { disconnectMySQLClient } from '../../../../services/RDSAuroraMySQLProxyService';
+
+/**
+ * @deprecated Disconnect db is no longer to be used due to the use of ConnectionPool
+ */
+const disconnectMySQLDb = () => {
+  return disconnectMySQLClient();
+};
+
+export default disconnectMySQLDb;
--- src/utils/db/mysql/proxy/index.ts
+++ src/utils/db/mysql/proxy/index.ts
@@ -1,3 +1,4 @@
 export { default as disconnectDb } from './disconnectDb';
+export { default as disconnectMySQLDb } from './disconnectMySQLDb';
 export { default as getClient } from './getClient';
 export { default as query } from './query';
